c0aec2ae8d8a8c37e4c909887d45557dcdbca9d5
Giovani Reis <gi.giovani2005@gmail.com>
2026-05-06 23:39:42 -0300

    Login funcionando

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,12 +1,57 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PIM_TechTrust.Models;
+using PIM_TechTrust.Models.Enums;
+using PIMIII_CLICKTECK.Data;
 
 namespace PIMIII_CLICKTECK.Controllers
 {
     public class LoginController : Controller
     {
+        private readonly Context _context;
+
+        public LoginController(Context context)
+        {
+            _context = context;
+        }
+
+
         public IActionResult Index()
         {
             return View();
         }
-    }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index(string email, string senha)
+        {
+            var usuario = await _context.Usuarios.FirstOrDefaultAsync(u => u.Email == email);
+            if (usuario == null)
+            {
+                ViewBag.Erro = "Usuário não encontrado";
+                return View();
+            }
+
+            bool senhaCorreta = BCrypt.Net.BCrypt.Verify(senha, usuario.Senha);
+            if (senhaCorreta)
+            {
+                switch (usuario.Role)
+                {
+                    case Role.Cliente:
+                        return RedirectToAction("Index", "DashBoard");
+                    case Role.Tecnico:
+                        return RedirectToAction("Index", "DashBoard");
+                    case Role.Admin:
+                        return RedirectToAction("Index", "DashBoard");
+
+
+                }
+            }
+            ViewBag.Erro = "Usuario ou Senha inválidos";
+            ViewBag.Email = email;
+            ViewBag.Senha = senha;
+            return View();
+         }
+     }
 }

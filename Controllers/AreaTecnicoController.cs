12aea78e1065b52fc36a8c40f04e44ad4c7cc7a8
Giovani Reis <gi.giovani2005@gmail.com>
2026-05-15 01:15:00 -0300

    login do técnico parcialmente funcional

--- Controllers/AreaTecnicoController.cs
+++ Controllers/AreaTecnicoController.cs
@@ -2,7 +2,9 @@
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using PIM_TechTrust.Models;
+using PIM_TechTrust.Models.Enums;
 using PIMIII_CLICKTECK.Data;
+using PIMIII_CLICKTECK.Models.ViewModels;
 
 namespace PIMIII_CLICKTECK.Controllers
 {
@@ -14,153 +16,46 @@ namespace PIMIII_CLICKTECK.Controllers
         {
             _context = context;
         }
-
-        // GET: AreaTecnico
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
-            var context = _context.Atendimentos.Include(a => a.Cliente).Include(a => a.Tecnico);
-            return View(await context.ToListAsync());
-        }
-
-        // GET: AreaTecnico/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
+            var usuLogado = TempData["Tecnico"];
+            if (usuLogado == null)
             {
-                return NotFound();
+                return RedirectToAction("Index", "Login");
             }
+            TempData.Keep("Tecnico");
 
-            var atendimento = await _context.Atendimentos
-                .Include(a => a.Cliente)
-                .Include(a => a.Tecnico)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (atendimento == null)
+            int idUsuario = (int)usuLogado;
+            // Busca o usuário, o perfil dele e os serviços pendentes
+            var tecnicoLogado = _context.Usuarios
+                .Include(u => u.TecnicoPerfil)
+                .FirstOrDefault(u => u.Id == idUsuario); // ID do técnico logado
+
+            var model = new DashboardTecnicoViewModel
             {
-                return NotFound();
-            }
+                Tecnico = tecnicoLogado,
+                AtendimentosPendentes = _context.Atendimentos
+                    .Include(a => a.Cliente)
+                    .Where(a => a.Status == StatusAtendimento.Solicitado)
+                    .ToList()
+            };
 
-            return View(atendimento);
+            return View(model);
         }
 
-        // GET: AreaTecnico/Create
-        public IActionResult Create()
-        {
-            ViewData["ClienteId"] = new SelectList(_context.Usuarios, "Id", "Id");
-            ViewData["TecnicoId"] = new SelectList(_context.Usuarios, "Id", "Id");
-            return View();
-        }
-
-        // POST: AreaTecnico/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,ClienteId,TecnicoId,ValorOrcamento,ObservacaoTecnico,Descricao,Status,DataAbertura,DataConclusao")] Atendimento atendimento)
+        public IActionResult EnviarOrcamento(int AtendimentoId, decimal Valor, string Observacao)
         {
-            if (ModelState.IsValid)
-            {
-                _context.Add(atendimento);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["ClienteId"] = new SelectList(_context.Usuarios, "Id", "Id", atendimento.ClienteId);
-            ViewData["TecnicoId"] = new SelectList(_context.Usuarios, "Id", "Id", atendimento.TecnicoId);
-            return View(atendimento);
-        }
-
-        // GET: AreaTecnico/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var atendimento = await _context.Atendimentos.FindAsync(id);
-            if (atendimento == null)
-            {
-                return NotFound();
-            }
-            ViewData["ClienteId"] = new SelectList(_context.Usuarios, "Id", "Id", atendimento.ClienteId);
-            ViewData["TecnicoId"] = new SelectList(_context.Usuarios, "Id", "Id", atendimento.TecnicoId);
-            return View(atendimento);
-        }
-
-        // POST: AreaTecnico/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,ClienteId,TecnicoId,ValorOrcamento,ObservacaoTecnico,Descricao,Status,DataAbertura,DataConclusao")] Atendimento atendimento)
-        {
-            if (id != atendimento.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(atendimento);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!AtendimentoExists(atendimento.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["ClienteId"] = new SelectList(_context.Usuarios, "Id", "Id", atendimento.ClienteId);
-            ViewData["TecnicoId"] = new SelectList(_context.Usuarios, "Id", "Id", atendimento.TecnicoId);
-            return View(atendimento);
-        }
-
-        // GET: AreaTecnico/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var atendimento = await _context.Atendimentos
-                .Include(a => a.Cliente)
-                .Include(a => a.Tecnico)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (atendimento == null)
-            {
-                return NotFound();
-            }
-
-            return View(atendimento);
-        }
-
-        // POST: AreaTecnico/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var atendimento = await _context.Atendimentos.FindAsync(id);
+            var atendimento = _context.Atendimentos.Find(AtendimentoId);
             if (atendimento != null)
             {
-                _context.Atendimentos.Remove(atendimento);
+                atendimento.ValorOrcamento = Valor;
+                atendimento.ObservacaoTecnico = Observacao;
+                atendimento.Status = StatusAtendimento.PendenteAprovacao;
+                _context.SaveChanges();
             }
-
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            return RedirectToAction("Index");
         }
 
-        private bool AtendimentoExists(int id)
-        {
-            return _context.Atendimentos.Any(e => e.Id == id);
-        }
     }
 }
---
+++ Models/ViewModels/DashboardTecnicoViewModel.cs
@@ -0,0 +1,14 @@
+﻿using PIM_TechTrust.Models;
+
+namespace PIMIII_CLICKTECK.Models.ViewModels
+{
+    public class DashboardTecnicoViewModel
+    {
+        public Usuario Tecnico { get; set; }
+        public List<Atendimento> AtendimentosPendentes { get; set; }
+
+        public decimal GanhosTotais { get; set; }
+        public int TotalReparos { get; set; }
+        public double AvaliacaoMedia { get; set; }
+    }
+}
